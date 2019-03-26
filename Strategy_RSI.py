b5250ad69cf402316834a1a7eaf3e721595604b8
kamilbl <48943052+kamilbl@users.noreply.github.com>
2019-03-26 09:12:45 +0100

    Update Strategy_RSI.py

--- Strategy_RSI.py
+++ Strategy_RSI.py
@@ -69,8 +69,8 @@ def StrategyRSI():
         qua = str(qua)[0:le]
       print(colorize(42,1,30,"\tCreate new order [BUY]\n\t") + str(Symbols.SymbolsMatrix[a][0])+"BTC RSI =\t"  + str(rsi) + "\n\tFree budget =\t" + str(budget_total) + "\n\tPrice =\t" + str(price) + "\n\tQuantity =\t" + str(qua))
       bot.send_message(chat_id=key.chat_id, text="\t Create new order [BUY] " + str(Symbols.SymbolsMatrix[a][0])+"BTC "  + str(rsi) + "\t Free budget: " + str(budget_total) + "\n Price: \t" + str(price) + "\n Quantity: \t" + str(qua))
-      #OrderBuy = client.create_order(symbol=str(Symbols.SymbolsMatrix[a][0]), side=client.SIDE_BUY, type=client.ORDER_TYPE_LIMIT, timeInForce=client.TIME_IN_FORCE_GTC, quantity=str(qua), price=str(price))
-      #print(str(OrderBuy))
+      OrderBuy = client.create_order(symbol=str(Symbols.SymbolsMatrix[a][0]), side=client.SIDE_BUY, type=client.ORDER_TYPE_LIMIT, timeInForce=client.TIME_IN_FORCE_GTC, quantity=str(qua), price=str(price))
+      print(str(OrderBuy))
       print("------------------------------------------------------------------------------------------------------------------------------------")
     elif float(rsi) > max:
       balanceALT = client.get_asset_balance(asset=str(Symbols.SymbolsMatrix[a][0]))
@@ -92,8 +92,8 @@ def StrategyRSI():
           qua = str(qua)[0:le]
         print("\t Create new order [SELL] " + str(Symbols.SymbolsMatrix[a][0])+"BTC "  + str(rsi) + "Free budget: " + str(budget_total) + "\n Price: \t" + str(price) + "\n Quantity: \t" + str(qua))
         bot.send_message(chat_id=key.chat_id, text="\t Create new order [SELL] " + str(Symbols.SymbolsMatrix[a][0])+"BTC "  + str(rsi) + "Free budget: " + str(budget_total) + "\n\t Price: \t" + str(price) + "\n\t Quantity: \t" + str(qua))
-        #OrderSell = client.create_order(symbol=str(Symbols.SymbolsMatrix[a][0]), side=client.SIDE_SELL, type=client.ORDER_TYPE_LIMIT, timeInForce=client.TIME_IN_FORCE_GTC, quantity=str(qua), price=str(price))
-        #print(str(OrderSell))
+        OrderSell = client.create_order(symbol=str(Symbols.SymbolsMatrix[a][0]), side=client.SIDE_SELL, type=client.ORDER_TYPE_LIMIT, timeInForce=client.TIME_IN_FORCE_GTC, quantity=str(qua), price=str(price))
+        print(str(OrderSell))
         print("------------------------------------------------------------------------------------------------------------------------------------")
       else: 
         T = "%-.8f" % (float(balanceALTFREE)*float(price))
